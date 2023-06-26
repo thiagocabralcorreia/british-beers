99e580e6b7d74d6ff46ec654ba542d7006c147e0
thiagocabralcorreia <thiagocabralcorreia@gmail.com>
2023-06-26 20:37:04 -0300

    feat: add DropdownMenuButton component with test

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { motion } from "framer-motion";
 import Image from "next/image";
 import Link from "next/link";
-
 import logo from "../public/logo.png";
 
 interface HeaderProps {
@@ -19,35 +18,40 @@ const Header = ({ childrenLeft, childrenRight, dropdownMenu }: HeaderProps) => {
       transition={{ ease: "easeInOut", duration: 0.9, delay: 0.2 }}
       className="fixed h-24 top-0 left-0 right-0 z-50"
     >
-      <nav className="flex content-center justify-between bg-primaryDark text-white">
-        <div className="flex items-center justify-between w-full mx-auto px-10 h-24">
-          <div className="hidden lg:flex">
-            <div className="flex items-baseline space-x-4 font-oswald text-lg">
-              {childrenLeft}
-            </div>
+      <nav
+        className="max-md:flex max-md:justify-between grid grid-cols-3 max-md:gap-2 gap-4 items-center
+        w-full mx-auto px-10 max-sm:px-5 h-24 bg-primaryDark text-white"
+      >
+        <div className="flex items-center justify-self-start">
+          <div className="flex items-baseline space-x-4 font-oswald">
+            {childrenLeft}
           </div>
+        </div>
 
-          <div className="flex items-center justify-between">
-            <div className="flex-shrink-0">
-              <Link href={"/"}>
-                <Image
-                  src={logo}
-                  alt="British Beers logo"
-                  className="h-16 w-auto"
-                />
-              </Link>
-            </div>
+        <div className="flex items-center justify-center col-span-1">
+          <div className="flex-shrink-0">
+            <Link href={"/"}>
+              <Image
+                src={logo}
+                alt="British Beers logo"
+                className="h-16 w-auto"
+              />
+            </Link>
           </div>
+        </div>
 
-          <div className="hidden lg:flex">
-            <div className="flex items-baseline space-x-4 font-oswald text-lg">
-              {childrenRight}
-            </div>
+        <div className="flex items-center justify-end col-span-1">
+          <div
+            className={`flex items-baseline space-x-4 font-oswald ${
+              childrenRight ? "" : "justify-self-end"
+            }`}
+          >
+            {childrenRight}
           </div>
         </div>
       </nav>
 
-      <>{dropdownMenu}</>
+      {dropdownMenu}
     </motion.header>
   );
 };
--- components/layout.tsx
+++ components/layout.tsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from "react";
 import Link from "next/link";
+import { MdOutlineSearch, MdPerson, MdShoppingCart } from "react-icons/md";
 
 import Header from "./Header";
 
 import { ILink, links } from "../data/links";
+import DropdownMenuButton from "./DropdownMenuButton";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   const [isOpen, setIsOpen] = useState(false);
@@ -28,36 +30,49 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     <div>
       <Header
         childrenLeft={
-          <>
+          <div className="flex content-center">
+            <DropdownMenuButton
+              onClick={() => setIsOpen(!isOpen)}
+              isOpen={isOpen}
+            />
             {links.map(({ id, href, name }: ILink) => {
               return (
                 <Link
                   key={id}
                   href={href}
-                  className="header-link block px-3 py-4 transition ease-out duration-900 hover:text-tertiary"
+                  className="max-lg:hidden header-link block px-3 py-4 transition ease-out duration-900 hover:text-tertiary"
                 >
                   {name?.toUpperCase()}
                 </Link>
               );
             })}
-          </>
+          </div>
         }
         childrenRight={
           <>
-            {links.map(({ id, href, name }: ILink) => {
-              return (
-                <Link
-                  key={id}
-                  href={href}
-                  className="header-link block px-3 py-4 transition ease-out duration-900 hover:text-tertiary"
-                >
-                  {name?.toUpperCase()}
-                </Link>
-              );
-            })}
+            <div className="flex space-x-4 max-sm:space-x-1">
+              <div className="flex items-center justify-center w-8 h-8">
+                <MdOutlineSearch
+                  className="text-xl transition-colors duration-300
+                cursor-pointer text-white hover:text-tertiary"
+                />
+              </div>
+              <div className="flex items-center justify-center w-8 h-8">
+                <MdPerson
+                  className="text-xl transition-colors duration-300
+                cursor-pointer text-white hover:text-tertiary "
+                />
+              </div>
+              <div className="flex items-center justify-center w-8 h-8">
+                <MdShoppingCart
+                  className="text-xl transition-colors duration-300
+                cursor-pointer text-white hover:text-tertiary "
+                />
+              </div>
+            </div>
           </>
         }
-        dropdownMenu={null}
+        dropdownMenu={<div className="flex font-oswald text-lg"></div>}
       />
       <div>{children}</div>
     </div>
