2c920a0944e5f509e46b2639137fcb0450a505df
thiagocabralcorreia <thiagocabralcorreia@gmail.com>
2024-06-02 14:49:12 -0300

    ✨ feat: create cart context

--- src/app/(store)/layout.tsx
+++ src/app/(store)/layout.tsx
@@ -1,11 +1,14 @@
-import { Header } from "@/components/header";
 import { ReactNode } from "react";
+import { CartProvider } from "@/context/cart-context";
+import { Header } from "@/components/header";
 
 export default function StoreLayout({ children }: { children: ReactNode }) {
   return (
-    <div className="mx-auto grid min-h-screen w-full max-w-[1600px] grid-rows-app gap-5">
-      <Header />
-      {children}
-    </div>
+    <CartProvider>
+      <div className="mx-auto grid min-h-screen w-full max-w-[1600px] grid-rows-app gap-5">
+        <Header />
+        {children}
+      </div>
+    </CartProvider>
   );
 }
