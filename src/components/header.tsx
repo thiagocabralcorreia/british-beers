b07a33d7cf9b028affb76e6dd37f514894759fef
thiagocabralcorreia <thiagocabralcorreia@gmail.com>
2024-06-03 19:56:31 -0300

    ✨ feat: add search form component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
-import { Search } from "lucide-react";
 import Image from "next/image";
 import { CartWidget } from "./cart-widget";
+import { SearchForm } from "./search-form";
 import avatar from "../../public/avatar.jpg";
 
 export function Header() {
@@ -12,14 +12,7 @@ export function Header() {
           British Beers
         </Link>
 
-        <form className="flex w-[320px] items-center gap-3 rounded-md bg-zinc-900 px-5 py-3 ring-zinc-700">
-          <Search className="w-5 h-5 text-zinc-500" />
-
-          <input
-            placeholder="Search"
-            className="flex-1 bg-transparent text-sm outline-none placeholder:text-zinc-500"
-          />
-        </form>
+        <SearchForm />
       </div>
 
       <div className="flex items-center gap-4">
